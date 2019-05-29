f0070c6984c3cc066546afc97ba93af956a758b6
vhnegrisoli <victorhugonegrisoli.ccs@hotmail.com>
2019-05-29 20:12:26 -0300

    Divide pastas do projeto

---
+++ main.py
@@ -0,0 +1,36 @@
+import time
+from regras_jogo import construir_jogo
+from agentes import construir_agente
+
+def ler_tempo(em_turnos=False):
+    """ Se o jogo for em turnos, passe 1 (rodada), senão se o jogo for
+    continuo ou estratégico, precisa.
+    """
+    return 1 if em_turnos else time.time()
+
+
+def iniciar_jogo():
+    
+    # Inicializar e configurar jogo
+    jogo = construir_jogo()
+    id_jogador, jogador = jogo.registrarAgenteJogador(), construir_agente()
+    tempo_de_jogo = 0
+
+    while not jogo.isFim():
+        
+        # Mostrar mundo ao jogador
+        ambiente_perceptivel = jogo.gerarCampoVisao(id_jogador)
+        jogador.adquirirPercepcao(ambiente_perceptivel)
+        
+        # Decidir jogada e apresentar ao jogo
+        acao = jogador.escolherProximaAcao()
+        jogo.registrarProximaAcao(id_jogador, acao)
+
+        # Atualizar jogo
+        tempo_corrente = ler_tempo()
+        jogo.atualizarEstado(tempo_corrente - tempo_de_jogo)
+        tempo_de_jogo += tempo_corrente
+
+
+if __name__ == '__main__':
+    iniciar_jogo()
---
+++ regras_jogo/regras_jogo.py
@@ -0,0 +1,51 @@
+from abc import ABC
+
+class RegrasJogo(ABC):
+    """ Interface mínima para implementar um jogo interativo e modular. Não
+    tente instanciar objetos dessa classe, ela deve ser herdada e seus métodos
+    abstratos sobrecarregados.
+    """
+
+    @abstractmethod
+    def registrarAgenteJogador(self, elem_agente=JOGADOR_PADRAO):
+        """ Cria ou recupera id de um elemento de jogo agente.
+        """
+        return
+    
+    @abstractmethod
+    def isFim(self):
+        """ Boolean indicando fim de jogo em True.
+        """
+        return
+
+    @abstractmethod
+    def gerarCampoVisao(self, id_agente):
+        """ Retorna um EstadoJogoView para ser consumido por um agente
+        específico. Objeto deve conter apenas descrição de elementos visíveis
+        para este agente.
+
+        EstadoJogoView é um objeto imutável ou uma cópia do jogo, de forma que
+        sua manipulação direta não tem nenhum efeito no mundo de jogo real.
+        """
+        return
+
+    @abstractmethod
+    def registrarProximaAcao(self, id_agente, acao):
+        """ Informa ao jogo qual a ação de um jogador especificamente.
+        Neste momento, o jogo ainda não é transformado em seu próximo estado,
+        isso é feito no método de atualização do mundo.
+        """
+        return
+    
+    @abstractmethod
+    def atualizarEstado(self, diferencial_tempo):
+        """ Apenas neste momento o jogo é atualizado para seu próximo estado
+        de acordo com as ações de cada jogador registradas anteriormente.
+        """
+        return
+
+def construir_jogo(*args,**kwargs):
+    """ Método factory para uma instância Regras arbitrária, de acordo com os
+    parâmetros. Pode-se mudar à vontade a assinatura do método.
+    """
+    pass
